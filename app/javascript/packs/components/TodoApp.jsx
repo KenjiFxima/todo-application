6cdce540367ba374d9611131e9f30b01fa0e2963
KenjiFxima <fukuken1207@gmail.com>
2021-07-03 19:38:51 +0900

    インデント直し

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -4,27 +4,27 @@ import axios from "axios";
 import TodoItems from "./TodoItems"
 import TodoItem from "./TodoItem"
 class TodoApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            todoItems: []
-        };
-        this.getTodoItems = this.getTodoItems.bind(this);
+  constructor(props) {
+      super(props);
+      this.state = {
+          todoItems: []
+      };
+      this.getTodoItems = this.getTodoItems.bind(this);
     }
-    componentDidMount() {
-        this.getTodoItems();
-    }
-    getTodoItems() {
-        axios
-        .get("/api/v1/todo_items")
-        .then(response => {
-            const todoItems = response.data;
-            this.setState({ todoItems });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
+  componentDidMount() {
+      this.getTodoItems();
+  }
+  getTodoItems() {
+      axios
+      .get("/api/v1/todo_items")
+      .then(response => {
+          const todoItems = response.data;
+          this.setState({ todoItems });
+      })
+      .catch(error => {
+          console.log(error);
+      });
+  }
   render() {
     return (
         <TodoItems>

353fcb86af731ab4881ea7d2db1b0da716aef01c
KenjiFxima <fukuken1207@gmail.com>
2021-07-04 21:08:09 +0900

    完了済みTodoを表示しない切り替えモードボタンの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,6 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-require("./components/TodoApp");
+require("./components/TodoApp")
 require('bootstrap')
 import 'bootstrap/dist/css/bootstrap'
--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -1,46 +1,59 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import axios from "axios";
+import axios from "axios"
+
 import TodoItems from "./TodoItems"
 import TodoItem from "./TodoItem"
-import TodoForm from "./TodoForm";
+import TodoForm from "./TodoForm"
 class TodoApp extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      todoItems: []
-    };
-    this.getTodoItems = this.getTodoItems.bind(this);
-    this.createTodoItem = this.createTodoItem.bind(this);
+      todoItems: [],
+      hideCompletedTodoItems: false
+    }
+    this.getTodoItems = this.getTodoItems.bind(this)
+    this.createTodoItem = this.createTodoItem.bind(this)
+    this.toggleCompletedTodoItems = this.toggleCompletedTodoItems.bind(this)
   }
   componentDidMount() {
-    this.getTodoItems();
+    this.getTodoItems()
   }
   getTodoItems() {
     axios
       .get("/api/v1/todo_items")
       .then(response => {
-        const todoItems = response.data;
-        this.setState({ todoItems });
+        const todoItems = response.data
+        this.setState({ todoItems })
       })
       .catch(error => {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
   createTodoItem(todoItem) {
-    const todoItems = [todoItem, ...this.state.todoItems];
-    this.setState({ todoItems });
+    const todoItems = [todoItem, ...this.state.todoItems]
+    this.setState({ todoItems })
+  }
+  toggleCompletedTodoItems() {
+    console.log()
+    this.setState({
+      hideCompletedTodoItems: !this.state.hideCompletedTodoItems
+    })
   }
   render() {
     return (
       <>
         <TodoForm createTodoItem={this.createTodoItem} />
-        <TodoItems>
+        <TodoItems
+          toggleCompletedTodoItems={this.toggleCompletedTodoItems}
+          hideCompletedTodoItems={this.state.hideCompletedTodoItems}
+        >
           {this.state.todoItems.map(todoItem => (
             <TodoItem 
               key={todoItem.id} 
               todoItem={todoItem} 
               getTodoItems={this.getTodoItems} 
+              hideCompletedTodoItems={this.state.hideCompletedTodoItems}
             />
           ))}
         </TodoItems>
